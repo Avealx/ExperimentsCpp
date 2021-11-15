76190d888cc7877ded789f1884ab0dc75f486108
Felix Herter <Lelix@localhost.localdomain>
2021-11-15 07:24:38 +0100

    clean CRPT - add description and clean up

--- CRTP.cpp
+++ CRTP.cpp
@@ -1,9 +1,17 @@
-// CRTP (curiously recurring template pattern)
-// static polymorphism
-// see https://www.grimm-jaud.de/index.php/blog/c-ist-doch-lazy
+/* CRTP (curiously recurring template pattern)
+ *
+ * static polymorphism
+ *
+ * motivator://www.grimm-jaud.de/index.php/blog/c-ist-doch-lazy
+ * Supply functionality through a base class.
+ * The base class implements the shared functionality by using methods that
+ * the derived class must supply.
+ * The base class uses static polymorphism to dispatch the impl() call.
+ */
 
 #include <iostream>
 using namespace std;
+
 
 template <typename Derived>
 struct Base {
@@ -11,18 +19,19 @@ struct Base {
     void impl() { cout << "Base!\n"; }
 };
 
-struct Derived1 : public Base<Derived1> {
+struct Derived1 : Base<Derived1> {
     void impl() { cout << "Derived1!\n"; }
 };
 
-struct Derived2 : public Base<Derived2> {
+struct Derived2 : Base<Derived2> {
     void impl() { cout << "Derived2!\n"; }
 };
  
-struct Derived3 : public Base<Derived3> {};
+struct Derived3 : Base<Derived3> {};
  
 template <typename T>
 void execute(T&& base) { base.interface(); }
+
 
 int main() {
     execute(Derived1{}); // Derived1
