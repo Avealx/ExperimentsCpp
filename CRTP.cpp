20c91653f155f8470d5506549c828ab6f94e321a
Felix Herter <herterfe@gmail.com>
2021-12-18 22:48:20 +0100

    refact crtp
    Rephrase the description and change the general layout a bit.

--- CRTP.cpp
+++ CRTP.cpp
@@ -3,12 +3,12 @@
  * static polymorphism
  *
  * motivator://www.grimm-jaud.de/index.php/blog/c-ist-doch-lazy
- * Supply functionality through a base class.
- * The base class implements the shared functionality by using methods that
- * the derived class must supply.
- * The base class uses static polymorphism to dispatch the impl() call.
- */
-
+ *
+ * Inject functionality through a base class template whose template parameter
+ * is the derived class (thus, recurring).
+ * The base class implements the functionality by applying methods that
+ * the derived class must implement. The base class uses static polymorphism to
+ * dispatch the impl() call. */
 #include <iostream>
 using namespace std;
 
@@ -19,22 +19,17 @@ struct Base {
     void impl() { cout << "Base!\n"; }
 };
 
-struct Derived1 : Base<Derived1> {
-    void impl() { cout << "Derived1!\n"; }
-};
 
-struct Derived2 : Base<Derived2> {
-    void impl() { cout << "Derived2!\n"; }
-};
- 
+struct Derived1 : Base<Derived1> { void impl() { cout << "Derived1!\n"; } };
+struct Derived2 : Base<Derived2> { void impl() { cout << "Derived2!\n"; } };
 struct Derived3 : Base<Derived3> {};
- 
+
 template <typename T>
 void execute(T&& base) { base.interface(); }
 
 
 int main() {
-    execute(Derived1{}); // Derived1
-    execute(Derived2{}); // Derived2
-    execute(Derived3{}); // Base
+    execute(Derived1{}); // Derived1!
+    execute(Derived2{}); // Derived2!
+    execute(Derived3{}); // Base!
 }
